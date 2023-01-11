4b59a96961d9bda1f6ba21fc2e82a9a4e1979996
David Domínguez Blanco <david.dominguez.blanco.2001@gmail.com>
2023-01-11 14:12:35 +0100

    Ejercicio averiguar fecha los int truncan

---
+++ Fundamentos/Form03AveriguaFecha.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Fundamentos
+{
+    partial class Form03AveriguaFecha
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDay = new System.Windows.Forms.TextBox();
+            this.txtMonth = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.btnCalculateDate = new System.Windows.Forms.Button();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Día";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mes";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Año";
+            // 
+            // txtDay
+            // 
+            this.txtDay.Location = new System.Drawing.Point(89, 44);
+            this.txtDay.Name = "txtDay";
+            this.txtDay.Size = new System.Drawing.Size(74, 23);
+            this.txtDay.TabIndex = 3;
+            // 
+            // txtMonth
+            // 
+            this.txtMonth.Location = new System.Drawing.Point(89, 86);
+            this.txtMonth.Name = "txtMonth";
+            this.txtMonth.Size = new System.Drawing.Size(74, 23);
+            this.txtMonth.TabIndex = 4;
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(89, 125);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(74, 23);
+            this.txtYear.TabIndex = 5;
+            // 
+            // btnCalculateDate
+            // 
+            this.btnCalculateDate.Location = new System.Drawing.Point(188, 68);
+            this.btnCalculateDate.Name = "btnCalculateDate";
+            this.btnCalculateDate.Size = new System.Drawing.Size(75, 56);
+            this.btnCalculateDate.TabIndex = 6;
+            this.btnCalculateDate.Text = "Calcular día";
+            this.btnCalculateDate.UseVisualStyleBackColor = true;
+            this.btnCalculateDate.Click += new System.EventHandler(this.btnCalculateDate_Click);
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(89, 177);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(99, 15);
+            this.lblDate.TabIndex = 7;
+            this.lblDate.Text = "Dia de la semana:";
+            // 
+            // Form03AveriguaFecha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 450);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.btnCalculateDate);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.txtMonth);
+            this.Controls.Add(this.txtDay);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form03AveriguaFecha";
+            this.Text = "Form03AveriguaFecha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtDay;
+        private TextBox txtMonth;
+        private TextBox txtYear;
+        private Button btnCalculateDate;
+        private Label lblDate;
+    }
+}
---
+++ Fundamentos/Form03AveriguaFecha.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fundamentos
+{
+    public partial class Form03AveriguaFecha : Form
+    {
+        public Form03AveriguaFecha()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculateDate_Click(object sender, EventArgs e)
+        {
+            int day = int.Parse(this.txtDay.Text);
+            int month = int.Parse(this.txtMonth.Text);
+            int year = int.Parse(this.txtYear.Text);
+
+            string dayweek ="";
+
+            if (month == 1) 
+            {
+                month = 13;
+                year -=1;
+            }
+
+            if (month == 2)
+            {
+                month = 14;
+                year -= 1;
+            }
+
+            int op1, op2, op3, op4,op5,op6,op7;
+
+            op1 = ((month + 1) * 3) / 5;
+            op2 = year / 4;
+            op3 = year / 100;
+            op4 = year / 400;
+            op5 = day + (month * 2) + year + op1 + op2 - op3 + op4 + 2;
+            op6 = op5 / 7;
+            op7 = op5 - (op6 * 7);
+
+            switch (op7) {
+                case 0:  dayweek = "Sábado"; break;
+                case 1:  dayweek = "Domingo"; break;
+                case 2:  dayweek = "Lunes"; break;
+                case 3:  dayweek = "Martes"; break;
+                case 4:  dayweek = "Miércoles"; break;
+                case 5:  dayweek = "Jueves"; break;
+                case 6:  dayweek = "Viernes"; break;
+            }
+
+            this.lblDate.Text = dayweek;
+        }
+    }
+}
