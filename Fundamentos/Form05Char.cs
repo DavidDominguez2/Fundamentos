635e6a951230aee1670f1857ccf23f4d0e93a0f1
David Domínguez Blanco <david.dominguez.blanco.2001@gmail.com>
2023-01-12 09:49:14 +0100

    CLASE Char

---
+++ Fundamentos/Form05Char.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fundamentos
+{
+    public partial class Form05Char : Form
+    {
+        public Form05Char()
+        {
+            InitializeComponent();
+        }
+
+        private void btnASCII_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i <=255; i++)
+            {
+                char c = (char)i;
+                if (char.IsLetter(c))
+                {
+                    this.txtLetras.Text += c;
+                }else if (char.IsNumber(c))
+                {
+                    this.txtNumeros.Text += c;
+                }else if (char.IsSymbol(c))
+                {
+                    this.txtSimbolos.Text += c;
+                }else if (char.IsPunctuation(c))
+                {
+                    this.txtPuntuacion.Text += c;
+                }
+            }
+        }
+    }
+}
