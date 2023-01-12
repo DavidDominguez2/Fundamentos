9c6a58572113588360fc3d85559263b05116d415
David Domínguez Blanco <david.dominguez.blanco.2001@gmail.com>
2023-01-12 11:29:57 +0100

    suma de numeros strings

---
+++ Fundamentos/Form07SumarNumeros.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Fundamentos
+{
+    partial class Form07SumarNumeros
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumeros = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.lblSum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNumeros
+            // 
+            this.txtNumeros.Location = new System.Drawing.Point(44, 67);
+            this.txtNumeros.Name = "txtNumeros";
+            this.txtNumeros.Size = new System.Drawing.Size(260, 34);
+            this.txtNumeros.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Introduzca números";
+            // 
+            // btnSum
+            // 
+            this.btnSum.Location = new System.Drawing.Point(78, 107);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(180, 41);
+            this.btnSum.TabIndex = 2;
+            this.btnSum.Text = "Sumar Números";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // lblSum
+            // 
+            this.lblSum.AutoSize = true;
+            this.lblSum.Location = new System.Drawing.Point(44, 200);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(54, 28);
+            this.lblSum.TabIndex = 3;
+            this.lblSum.Text = "label";
+            // 
+            // Form07SumarNumeros
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 253);
+            this.Controls.Add(this.lblSum);
+            this.Controls.Add(this.btnSum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumeros);
+            this.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form07SumarNumeros";
+            this.Text = "Form07SumarNumeros";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtNumeros;
+        private Label label1;
+        private Button btnSum;
+        private Label lblSum;
+    }
+}
---
+++ Fundamentos/Form07SumarNumeros.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fundamentos
+{
+    public partial class Form07SumarNumeros : Form
+    {
+        public Form07SumarNumeros()
+        {
+            InitializeComponent();
+            this.lblSum.Text = "";
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            string textoNumeros = this.txtNumeros.Text;
+            int result = 0;
+            for (int i = 0; i < textoNumeros.Length; i++)
+            {
+                char c = textoNumeros[i];
+                result += int.Parse(c.ToString());
+            }
+
+            this.lblSum.Text = "La suma es: " + result.ToString();
+        }
+    }
+}
