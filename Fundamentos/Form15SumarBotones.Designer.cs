18f0b4235be0bab2e758803af122733428ecdba8
David Domínguez Blanco <david.dominguez.blanco.2001@gmail.com>
2023-01-16 11:10:28 +0100

    Sumar Botones

---
+++ Fundamentos/Form15SumarBotones.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Fundamentos {
+    partial class Form15SumarBotones {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.btnReload = new System.Windows.Forms.Button();
+            this.pnlBtns = new System.Windows.Forms.Panel();
+            this.pnlBtns.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(279, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(279, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(279, 50);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 32);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(423, 50);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(114, 32);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(423, 98);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(114, 32);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(423, -1);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(114, 32);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 184);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(261, 29);
+            this.txtResult.TabIndex = 6;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(5, 50);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(114, 32);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(5, 98);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(114, 32);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(5, -1);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(114, 32);
+            this.button9.TabIndex = 7;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(142, 50);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(114, 32);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(142, 98);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(114, 32);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "button11";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(142, -1);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(114, 32);
+            this.button12.TabIndex = 10;
+            this.button12.Text = "button12";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // btnReload
+            // 
+            this.btnReload.Location = new System.Drawing.Point(303, 184);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(251, 32);
+            this.btnReload.TabIndex = 13;
+            this.btnReload.Text = "Reload";
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // pnlBtns
+            // 
+            this.pnlBtns.Controls.Add(this.button10);
+            this.pnlBtns.Controls.Add(this.button9);
+            this.pnlBtns.Controls.Add(this.button8);
+            this.pnlBtns.Controls.Add(this.button4);
+            this.pnlBtns.Controls.Add(this.button11);
+            this.pnlBtns.Controls.Add(this.button5);
+            this.pnlBtns.Controls.Add(this.button7);
+            this.pnlBtns.Controls.Add(this.button6);
+            this.pnlBtns.Controls.Add(this.button3);
+            this.pnlBtns.Controls.Add(this.button12);
+            this.pnlBtns.Controls.Add(this.button2);
+            this.pnlBtns.Controls.Add(this.button1);
+            this.pnlBtns.Location = new System.Drawing.Point(12, 32);
+            this.pnlBtns.Name = "pnlBtns";
+            this.pnlBtns.Size = new System.Drawing.Size(542, 136);
+            this.pnlBtns.TabIndex = 14;
+            // 
+            // Form15SumarBotones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 240);
+            this.Controls.Add(this.pnlBtns);
+            this.Controls.Add(this.btnReload);
+            this.Controls.Add(this.txtResult);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form15SumarBotones";
+            this.Text = "Form15SumarBotones";
+            this.pnlBtns.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private TextBox txtResult;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button btnReload;
+        private Panel pnlBtns;
+    }
+}
---
+++ Fundamentos/Form15SumarBotones.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fundamentos {
+    public partial class Form15SumarBotones : Form {
+        List<Button> botones;
+        int sum;
+        public Form15SumarBotones() {
+            InitializeComponent();
+            botones = new List<Button>();
+            sum = 0;
+
+            foreach (Control control in this.pnlBtns.Controls) {
+                if (control is Button) {
+                    this.botones.Add((Button)control);
+                }
+            }
+            Random rm = new Random();
+            foreach (Button btn in this.botones) {
+                int num = rm.Next(1, 100);
+                btn.Text = num.ToString();
+                btn.Click += suma;
+            }
+        }
+
+        private void suma(object? sender, EventArgs e) {
+            Button btn = (Button)sender;
+            sum += int.Parse(btn.Text);
+            this.txtResult.Text = sum.ToString();
+            btn.BackColor = Color.Coral;
+        }
+
+        private void btnReload_Click(object sender, EventArgs e) {
+            Random rm = new Random();
+            this.sum = 0;
+            this.txtResult.Text = "0";
+            foreach (Button btn in this.botones) {
+                int num = rm.Next(1, 100);
+                btn.Text = num.ToString();
+                btn.BackColor = Color.FromKnownColor(KnownColor.ButtonFace);
+            }
+
+        }
+    }
+}
