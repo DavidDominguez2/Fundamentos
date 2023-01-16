d994d27229bbfdfcb54f8bdd55c006038c0e9691
David Domínguez Blanco <david.dominguez.blanco.2001@gmail.com>
2023-01-16 11:42:47 +0100

    multi

--- Fundamentos/Form16TablaMultiplicar.Designer.cs
+++ Fundamentos/Form16TablaMultiplicar.Designer.cs
@@ -23,12 +23,172 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.btnShowTable = new System.Windows.Forms.Button();
+            this.gpbTable = new System.Windows.Forms.GroupBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.gpbTable.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(12, 33);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 29);
+            this.txtNum.TabIndex = 1;
+            // 
+            // btnShowTable
+            // 
+            this.btnShowTable.Location = new System.Drawing.Point(12, 68);
+            this.btnShowTable.Name = "btnShowTable";
+            this.btnShowTable.Size = new System.Drawing.Size(100, 61);
+            this.btnShowTable.TabIndex = 2;
+            this.btnShowTable.Text = "Mostrar Tabla";
+            this.btnShowTable.UseVisualStyleBackColor = true;
+            this.btnShowTable.Click += new System.EventHandler(this.btnShowTable_Click);
+            // 
+            // gpbTable
+            // 
+            this.gpbTable.Controls.Add(this.textBox9);
+            this.gpbTable.Controls.Add(this.textBox8);
+            this.gpbTable.Controls.Add(this.textBox7);
+            this.gpbTable.Controls.Add(this.textBox6);
+            this.gpbTable.Controls.Add(this.textBox5);
+            this.gpbTable.Controls.Add(this.textBox4);
+            this.gpbTable.Controls.Add(this.textBox3);
+            this.gpbTable.Controls.Add(this.textBox2);
+            this.gpbTable.Controls.Add(this.textBox1);
+            this.gpbTable.Location = new System.Drawing.Point(161, 33);
+            this.gpbTable.Name = "gpbTable";
+            this.gpbTable.Size = new System.Drawing.Size(178, 357);
+            this.gpbTable.TabIndex = 3;
+            this.gpbTable.TabStop = false;
+            this.gpbTable.Text = "Tabla Multiplicar";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(32, 175);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(109, 29);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(32, 140);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(109, 29);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(32, 105);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(109, 29);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(32, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(109, 29);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(109, 29);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(32, 210);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(109, 29);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(32, 245);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(109, 29);
+            this.textBox7.TabIndex = 6;
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(32, 280);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(109, 29);
+            this.textBox8.TabIndex = 7;
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(32, 315);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(109, 29);
+            this.textBox9.TabIndex = 8;
+            this.textBox9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form16TablaMultiplicar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(420, 402);
+            this.Controls.Add(this.gpbTable);
+            this.Controls.Add(this.btnShowTable);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form16TablaMultiplicar";
             this.Text = "Form16TablaMultiplicar";
+            this.gpbTable.ResumeLayout(false);
+            this.gpbTable.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox txtNum;
+        private Button btnShowTable;
+        private GroupBox gpbTable;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private TextBox textBox9;
+        private TextBox textBox8;
+        private TextBox textBox7;
+        private TextBox textBox6;
     }
 }
--- Fundamentos/Form16TablaMultiplicar.cs
+++ Fundamentos/Form16TablaMultiplicar.cs
@@ -10,8 +10,24 @@ using System.Windows.Forms;
 
 namespace Fundamentos {
     public partial class Form16TablaMultiplicar : Form {
+        List<TextBox> txtBoxes;
         public Form16TablaMultiplicar() {
             InitializeComponent();
+            this.txtBoxes = new List<TextBox>();
+
+            foreach (TextBox box in this.gpbTable.Controls) {
+                this.txtBoxes.Add(box);
+            }
+        }
+
+        private void btnShowTable_Click(object sender, EventArgs e) {
+            int num = int.Parse(this.txtNum.Text);
+            this.txtBoxes.Reverse();
+
+            for (int i = 0; i < this.txtBoxes.Count; i++) {
+                int mul = (i + 1) * num;
+                this.txtBoxes[i].Text = mul.ToString();
+            }
         }
     }
 }
